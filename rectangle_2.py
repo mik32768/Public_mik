f5b50513d583d5a3929c6659d77e08104b02c83e
mik32768 <79668150+mik32768@users.noreply.github.com>
2021-03-19 00:18:29 +0300

    Add files via upload

---
+++ rectangle_2.py
@@ -0,0 +1,26 @@
+from rectangle import Rectangle, Square, Circle
+
+#далее создаем два прямоугольника
+
+rect_1 = Rectangle(3,4)
+rect_2 = Rectangle(12,5)
+#вывод площади наших двух прямоугольников
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+print(square_1.get_area_square(),
+      square_2.get_area_square())
+
+figures = [rect_1, rect_2, square_1, square_2]
+
+for figure in figures:
+    if isinstance(figure, Square):
+        print(figure.get_area_square())
+    else:
+        print(figure.get_area())
+
+circle_1 = Circle(1)
+print(circle_1.area_circle())
